d5738c1d7d687e883ae55809d980cd54639e843b
IvanCastroRuiz <icastror@hotmail.com>
2021-11-23 19:23:58 -0500

    Ej Clases ReactJS MinTIC2022 Ciclo4 - Aplicaciones WEB

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+// Importamos el componente
+import MiComponente from './components/MiComponente';
+import Receta from './components/Receta';
 
+function presentacion(grupos, año){
+  const presentacion = <div>
+                            <h2>Desarrollo de aplicaciones WEB, {grupos} <br/></h2>
+                            <h3>Formador: Ivan Castro Ruiz</h3>
+                            <h4> {año} </h4>
+                      </div>
+  return presentacion;
+};
+// Componente principal que trae el FrameWorks
 function App() {
+  const grupos = 'NRC 53848 - 53849';
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {presentacion(grupos, 2021)}    
+        <section>
+          <MiComponente/>
+          <Receta/>
+        </section>   
       </header>
     </div>
   );
 }
-
 export default App;
