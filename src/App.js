41fded891171638d84e569ab58edcfa5f0b15de3
IvanCastroRuiz <icastror@hotmail.com>
2021-11-24 23:13:03 -0500

    Tercer commit (State - Props)

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import Header from './components/Header';
 import Slider from './components/Slider';
 import Sidebar from './components/Sidebar';
 import Footer from './components/Footer';
+import Estado from './components/Estado';
+import SliderProps from './components/SliderProps';
+import Peliculas from './components/Peliculas';
 
 function presentacion(grupos, año){
   const presentacion = <div>
@@ -21,31 +24,37 @@ function App() {
   return (
     <div className="App">
       <Header/>
-      
       <Slider/>
-
+      
      <div className="center">
-          
           <section id="content">
-
             <section className="componentes">
+              <h2>Funciones y JSX</h2>
               {presentacion(grupos, 2021)}    
+              <h2>Componentes</h2>
               <MiComponente/>
               <Receta/>
+              <h2>Estados (State)</h2>
+              <Estado/>
+              {/*<h2>Props </h2>
+              {Props}
+              <SliderProps
+                    title = "Bienvenido al Curso de ReactJS NRC 53848 - 53849"
+                    grupos= {grupos}
+              />*/}
+
+              <Peliculas/>
+
             </section>  
           </section>    
           <Sidebar/>
-
+          
           {/*LIMPIAR FLOTADOS*/}
           <div class="clearfix">
-                  
           </div>
-
       </div> {/* END DIV CENTER */}
       <Footer/>
     </div>
-
-
   );
 }
 export default App;
